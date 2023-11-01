f8b3ac356f9056242568478a05936162f601e996
Rewer2023 <mrkalling2017@gmail.com>
2023-11-01 21:38:25 +0200

    add new files

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main()
+        {
+            WriteTextWithBorder("Menu:");
+            WriteTextWithBorder("");
+            WriteTextWithBorder(" ");
+            WriteTextWithBorder("Game Over!");
+            WriteTextWithBorder("Select level:");
+        }
+
+        private static void WriteTextWithBorder(string text)
+        {
+            string a = "+";
+            for (int i = 0; i < text.Length + 2; i++)
+            {
+                a += ("-");
+            }
+            Console.WriteLine(a + "+");
+            Console.WriteLine("| " + text + " |");
+            Console.WriteLine(a + "+");
+        }
+    } 
+}
