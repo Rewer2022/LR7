39496f8fdac1a1146b689215dd9343384e0572bc
Rewer2022 <mrkalling2017@gmail.com>
2023-11-01 21:56:57 +0200

    Add new Line and swap + on =

--- Program.cs
+++ Program.cs
@@ -15,18 +15,19 @@ namespace HelloWorld
             WriteTextWithBorder(" ");
             WriteTextWithBorder("Game Over!");
             WriteTextWithBorder("Select level:");
+            WriteTextWithBorder("I'm changed that!!!");
         }
 
         private static void WriteTextWithBorder(string text)
         {
-            string a = "+";
+            string a = "=";
             for (int i = 0; i < text.Length + 2; i++)
             {
                 a += ("-");
             }
-            Console.WriteLine(a + "+");
+            Console.WriteLine(a + "=");
             Console.WriteLine("| " + text + " |");
-            Console.WriteLine(a + "+");
+            Console.WriteLine(a + "=");
         }
     } 
 }
